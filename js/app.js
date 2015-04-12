6073e960f924adf637e6b82cf402f7e3b1d06944
Jose Zamudio <zamudio@outlook.com>
2015-04-11 22:42:51 -0700

    Change path for credits

--- js/app.js
+++ js/app.js
@@ -4,5 +4,5 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
   this.route('about', { path: '/aboutus' });
-  this.route('credits', { path: '/credits' });
+  this.route('credits');
 });
